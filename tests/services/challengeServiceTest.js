fb7b3afbed68c33ce3e9d5e390126d9845e52d9d
Sean Dobberstein <sean.dobberstein@gmail.com>
2016-09-09 22:37:50 -0400

    Changing to use es6 arrow syntax

--- tests/services/challengeServiceTest.js
+++ tests/services/challengeServiceTest.js
@@ -3,33 +3,33 @@ var td = require('testdouble');
 var jsonFileLoader = td.replace('../../server/utils/jsonFileLoader');
 var challengeService = require('../../server/services/challengeService');
 
-test('can load all challenges', function(t) {
+test('can load all challenges', (t) => {
   // load fake data
   td.when(jsonFileLoader.loadJson('../data/challenges.json')).thenResolve([{ id: '1234' }, { id: '5678' }]);
 
-  challengeService.getAllChallenges().then(function(challenges) {
+  challengeService.getAllChallenges().then((challenges) => {
     t.deepEqual(challenges, [{ id: '1234' }, { id: '5678' }]);
     t.end();
     td.reset();
   });
 });
 
-test('can get challenge by id', function(t) {
+test('can get challenge by id', (t) => {
   // load fake data
   td.when(jsonFileLoader.loadJson('../data/challenges.json')).thenResolve([{ id: '1234' }, { id: '5678' }]);
 
-  challengeService.getChallengeById('1234').then(function(challenge) {
+  challengeService.getChallengeById('1234').then((challenge) => {
     t.deepEqual(challenge, { id: '1234' });
     t.end();
     td.reset();
   });
 });
 
-test('returns undefined when a challenge doesnt exist', function(t) {
+test('returns undefined when a challenge doesnt exist', (t) => {
   // load fake data
   td.when(jsonFileLoader.loadJson('../data/challenges.json')).thenResolve([{ id: '1234' }, { id: '5678' }]);
 
-  challengeService.getChallengeById('9012').then(function(challenge) {
+  challengeService.getChallengeById('9012').then((challenge) => {
     t.deepEqual(challenge, undefined);
     t.end();
     td.reset();
