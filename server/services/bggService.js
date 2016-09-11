c1cf765b28117a14bcd653bc0f21c24bb6d026c1
Sean Dobberstein <sean.dobberstein@gmail.com>
2016-09-10 23:22:05 -0400

    Add methods to get boardgame information

--- server/services/bggService.js
+++ server/services/bggService.js
@@ -1,10 +1,35 @@
 var _ = require('lodash');
 var bgg = require('bgg')();
+var Promise = require('bluebird');
+var cache = require('lru-cache')(100);
 
 var MAX_PAGES = 10;
 var PLAY_PARAMS = ['username', 'mindate', 'maxdate', 'page'];
 
 module.exports = {
+
+  boardgameName: function(id) {
+    var self = this;
+    return new Promise(function(resolve) {
+      if (cache.has(id)) {
+        resolve(cache.get(id));
+      } else {
+        self.boardgame(id).then(function(boardgame) {
+          var names = _.isArray(boardgame.name) ? boardgame.name : [boardgame.name];
+          var primaryName = _.find(names, function(name) { return name.type === 'primary'; });
+          cache.set(id, primaryName.value);
+          resolve(primaryName.value);
+        });
+      }
+    });
+  },
+
+  boardgame: function(id) {
+    return bgg('thing', { id, type: 'boardgame' })
+      .then(function(results) {
+        return results.items.item;
+      });
+  },
 
   plays: function(params) {
     var promises = [];
